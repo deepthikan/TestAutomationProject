f5b3251976f426db3ffbf458fa2953b154184af9
Deepthi <deepthikanchenadam@gmail.com>
2022-07-07 23:16:22 +1200

    Time and Material Module

--- Testautomationproject/Testautomationproject/Program.cs
+++ Testautomationproject/Testautomationproject/Program.cs
@@ -33,8 +33,47 @@ else
     Console.WriteLine("failed login,test failed");
 }
 
+// Go to administration and click time and material 
+IWebElement Administration = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a"));
+Administration.Click();
+    // create new time and material 
+IWebElement TimeAndMaterial = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+TimeAndMaterial.Click();
+//Identify create new
+IWebElement CreateNew = driver.FindElement(By.XPath("//*[@id='container']/p/a"));
+CreateNew.Click();
+Thread.Sleep(3000);
+//Input code Textbox
+IWebElement codeTextbox = driver.FindElement(By.Id("Code"));
+codeTextbox.SendKeys("Books");
+//Input Description Textbox
+IWebElement DescrptionTextbox = driver.FindElement(By.Id("Description"));
+DescrptionTextbox.SendKeys("Books2022");
+//Input Price per unit Textbox
+Thread.Sleep(1500);
+IWebElement Pricetag = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[4]/div/span[1]/span/input[1]"));
+Pricetag.Click();
+IWebElement price = driver.FindElement(By.Id("Price"));
+price.SendKeys("50");
+//Click on save button
+Thread.Sleep(2000);
+IWebElement SaveButton = driver.FindElement(By.Id("SaveButton"));
+SaveButton.Click();
+Thread.Sleep(5000);
 
+//Go to last page
+IWebElement GotoLastpage = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[4]/a[4]/span"));
+GotoLastpage.Click();
+IWebElement newcode = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[1]"));
 
+if (newcode.Text == "Books" )
+{
+    Console.WriteLine("New material has been created successfully");
+}
+else
+{
+    Console.WriteLine("New Material record hasn't been created");
+}
 
 
 
