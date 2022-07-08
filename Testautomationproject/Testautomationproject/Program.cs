26fc9ee4758bc1eb575bbb3f66ce8c9157a799b0
Deepthi <deepthikanchenadam@gmail.com>
2022-07-08 19:09:32 +1200

    Edit functionality for Time and Material Module

--- Testautomationproject/Testautomationproject/Program.cs
+++ Testautomationproject/Testautomationproject/Program.cs
@@ -64,18 +64,52 @@ Thread.Sleep(5000);
 //Go to last page
 IWebElement GotoLastpage = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[4]/a[4]/span"));
 GotoLastpage.Click();
+Thread.Sleep(3000);
 IWebElement newcode = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[1]"));
 
 if (newcode.Text == "Books" )
 {
-    Console.WriteLine("New material has been created successfully");
+    Console.WriteLine("New material has been created successfully.");
 }
 else
 {
-    Console.WriteLine("New Material record hasn't been created");
+    Console.WriteLine("New Material record hasn't been created.");
 }
 
+//click on edit button
 
+IWebElement Editbutton = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[5]/a[1]"));
+Editbutton.Click();
+Thread.Sleep(1500);
+//edit code textbox
 
+IWebElement editCodeButton = driver.FindElement(By.Id("Code"));
+editCodeButton.Clear();
+editCodeButton.SendKeys("Books Ed");
+//Edit Description
 
+IWebElement editDescription = driver.FindElement(By.Id("Description"));
+editDescription.Clear();
+editDescription.SendKeys("Books2022 Ed");
+//click Save Button
+
+driver.FindElement(By.Id("SaveButton")).Click();
+Thread.Sleep(3000);
+//Go to last page
+
+driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[4]/a[4]/span")).Click();
+
+//Validate updated Display message
+
+if (driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[1]")).Text.Equals("Books Ed"))
+{
+    Console.WriteLine("Updated Succesfully.");
+}
+else
+
+{
+    Console.WriteLine("Update failed.");
+}
+
+driver.Close();
 
